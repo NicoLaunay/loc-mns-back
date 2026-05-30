d69f06ebe55c9f31f064775a2edf870f7f3804e2
Nicolas <nicolas.launay.0@gmail.com>
2026-05-30 11:17:13 +0200

    EDIT(ModificationDao): added request to delete all modifications from a specific user

--- src/main/java/com/mns/cda/loc_mns/dao/ModificationDao.java
+++ src/main/java/com/mns/cda/loc_mns/dao/ModificationDao.java
@@ -2,10 +2,18 @@ package com.mns.cda.loc_mns.dao;
 
 import com.mns.cda.loc_mns.model.Location;
 import com.mns.cda.loc_mns.model.Modification;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ModificationDao extends JpaRepository<Modification, Integer> {
-
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Modification modif " +
+            "WHERE modif.author.id = :userId ")
+    void deleteAllByUserId(@Param("userId") int userId);
 }
