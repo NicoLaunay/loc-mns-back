844a9a4c6e3c44818fb7ccfb6e565f8acdc94ee9
Nicolas <nicolas.launay.0@gmail.com>
2026-06-01 10:42:10 +0200

    DELETE: deleted unused imports and problematic comment

--- src/test/java/com/mns/cda/loc_mns/integration/LocMnsApplicationTests.java
+++ src/test/java/com/mns/cda/loc_mns/integration/LocMnsApplicationTests.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -22,10 +19,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@RequiredArgsConstructor
 class LocMnsApplicationTests {
 
-    private final WebApplicationContext context;
+    @Autowired
+    private WebApplicationContext context;
+
     private ObjectMapper mapper = JsonMapper.builder().build(); // Utilisé dans le test de la méthode update
 
     private MockMvc mvc;
