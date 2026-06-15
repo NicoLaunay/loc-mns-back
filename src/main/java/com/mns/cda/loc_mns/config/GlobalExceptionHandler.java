4072ffd70c1d489cd48c2081cca3d3c19f4db47e
Nicolas <nicolas.launay.0@gmail.com>
2026-06-15 14:16:17 +0200

    FIX: exception handler now ignores actuator exceptions

--- src/main/java/com/mns/cda/loc_mns/config/GlobalExceptionHandler.java
+++ src/main/java/com/mns/cda/loc_mns/config/GlobalExceptionHandler.java
@@ -39,6 +39,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneric(Exception e) {
 
+        // On ignore les exceptions liées à Actuator
+        if (e.getMessage() != null && e.getMessage().contains("actuator")) {
+            throw new RuntimeException(e);
+        }
+
         System.out.println("exception : " + e.getMessage());
 
         return ResponseEntity
