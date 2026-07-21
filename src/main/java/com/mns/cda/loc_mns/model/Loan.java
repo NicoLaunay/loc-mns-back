f56c5a0ebd2180c3eb57e8d7971c6ff1530c77ea
Nicolas <nicolas.launay.0@gmail.com>
2026-07-21 17:28:54 +0200

    CHORE: deleted unused import

--- src/main/java/com/mns/cda/loc_mns/model/Loan.java
+++ src/main/java/com/mns/cda/loc_mns/model/Loan.java
@@ -13,7 +13,6 @@ import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 @Getter // Crée les Getters
 @Setter // Crée les Setters
